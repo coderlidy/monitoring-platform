6eaf5c0e83303a2140be1c60716fe6c3a868d083
coderlidy <448834752@qq.com>
2020-04-10 21:02:15 +0800

    AOP:@ComponentScan代替@ImportResource和aop.xml配置

---
+++ src/main/java/com/car/demo/model/Log.java
@@ -0,0 +1,69 @@
+package com.car.demo.model;
+
+import java.sql.Timestamp;
+
+public class Log {
+    private Long id;
+    private Long operator;
+    private String object;
+    private String code;
+    private String describe;
+    private String ip;
+    private Timestamp gmtCreate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Long operator) {
+        this.operator = operator;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Timestamp getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Timestamp gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+}
