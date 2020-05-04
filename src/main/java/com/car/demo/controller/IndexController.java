dcb1e31c0316baac2932067b19bb5aca61c39a0d
coderlidy <448834752@qq.com>
2020-05-04 14:53:38 +0800

    readme.md

--- src/main/java/com/car/demo/controller/IndexController.java
+++ src/main/java/com/car/demo/controller/IndexController.java
@@ -5,6 +5,7 @@ import com.car.demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
