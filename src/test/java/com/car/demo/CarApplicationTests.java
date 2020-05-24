58c6deff8ff9447eb8f8d79301c422b328732455
coderlidy <448834752@qq.com>
2020-05-24 18:57:30 +0800

    基本的redis/修复测试代码运行时Tomcat没有打开使得websocket无效

--- src/test/java/com/car/demo/CarApplicationTests.java
+++ src/test/java/com/car/demo/CarApplicationTests.java
@@ -6,10 +6,13 @@ import com.car.demo.mapper.LogMapper;
 import com.car.demo.model.Cranenow;
 import com.car.demo.model.Log;
 import com.car.demo.model.Position;
+import com.car.demo.redis.MyRedisUtils;
 import com.car.demo.service.PasswordCodeService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
@@ -18,8 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Stack;
-
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CarApplicationTests {
     @Autowired
     private LogMapper logMapper;
@@ -29,9 +31,28 @@ class CarApplicationTests {
         Log log=new Log();
         log.setIp("hhh");
         log.setGmtCreate(new Timestamp(System.currentTimeMillis()));
-        System.out.println(logMapper.insert(log));
+        //System.out.println(logMapper.insert(log));
         int i=1/1;
         //int i=1/0;
     }
 
+    @Autowired
+    private MyRedisUtils redisUtils;
+
+    /**
+     * 插入缓存数据
+     */
+    @Test
+    public void set() {
+        redisUtils.set("redis_key", "redis_vale");
+    }
+
+    /**
+     * 读取缓存数据
+     */
+    @Test
+    public void get() {
+        String value = redisUtils.get("redis_key");
+        System.out.println(value);
+    }
 }
